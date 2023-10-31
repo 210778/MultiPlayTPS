9352dfc775704d996113d526801ca0a457c60a40
210778 <210778@jc-21.jp>
2023-10-31 13:16:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Intermediate/Build/Win64/ThirdPersonMPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.35.32216
+#include "E:/210778/04UnrealEngine/ThirdPersonMP/Intermediate/Build/Win64/ThirdPersonMPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/ThirdPersonMP/ThirdPersonMPCharacter.generated.h
@@ -0,0 +1,97 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef THIRDPERSONMP_ThirdPersonMPCharacter_generated_h
+#error "ThirdPersonMPCharacter.generated.h already included, missing '#pragma once' in ThirdPersonMPCharacter.h"
+#endif
+#define THIRDPERSONMP_ThirdPersonMPCharacter_generated_h
+
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_SPARSE_DATA
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_RPC_WRAPPERS
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAThirdPersonMPCharacter(); \
+	friend struct Z_Construct_UClass_AThirdPersonMPCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AThirdPersonMPCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ThirdPersonMP"), NO_API) \
+	DECLARE_SERIALIZER(AThirdPersonMPCharacter)
+
+
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAThirdPersonMPCharacter(); \
+	friend struct Z_Construct_UClass_AThirdPersonMPCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AThirdPersonMPCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ThirdPersonMP"), NO_API) \
+	DECLARE_SERIALIZER(AThirdPersonMPCharacter)
+
+
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AThirdPersonMPCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AThirdPersonMPCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AThirdPersonMPCharacter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AThirdPersonMPCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AThirdPersonMPCharacter(AThirdPersonMPCharacter&&); \
+	NO_API AThirdPersonMPCharacter(const AThirdPersonMPCharacter&); \
+public:
+
+
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AThirdPersonMPCharacter(AThirdPersonMPCharacter&&); \
+	NO_API AThirdPersonMPCharacter(const AThirdPersonMPCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AThirdPersonMPCharacter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AThirdPersonMPCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AThirdPersonMPCharacter)
+
+
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(AThirdPersonMPCharacter, CameraBoom); } \
+	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(AThirdPersonMPCharacter, FollowCamera); }
+
+
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_9_PROLOG
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_SPARSE_DATA \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_RPC_WRAPPERS \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_INCLASS \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_SPARSE_DATA \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> THIRDPERSONMP_API UClass* StaticClass<class AThirdPersonMPCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ThirdPersonMP_Source_ThirdPersonMP_ThirdPersonMPCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
